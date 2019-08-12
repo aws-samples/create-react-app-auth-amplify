cf8da3fb0d0364bfa09652aa41f7e2e681e4ee66
swaut <54040172+swaut@users.noreply.github.com>
2019-08-12 21:24:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './swaut.jpg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
@@ -13,15 +13,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to test reload.
+            Welcome to Swaut Automation.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.swaut.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Swaut Automation
           </a>
         </header>
       </div>
