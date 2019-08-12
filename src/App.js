efad7c1707b4765acaac39e9ce77ef1a99ca07ef
swaut <54040172+swaut@users.noreply.github.com>
2019-08-12 21:55:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} alt="Swaut Automation" />
           <p>
             Welcome to Swaut Automation.
           </p>
