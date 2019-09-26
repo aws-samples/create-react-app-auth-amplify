8466bff1d77c01fb4c18a164300176c6fefb4249
Karthick S <kartick.ns@gmail.com>
2019-09-26 12:14:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+      GyanTalk
           </p>
           <a
             className="App-link"
