64c976e81b26dc5664becdc885122e999e5bf755
Alistair Blair <ablair08@me.com>
2020-04-14 21:49:06 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -12,17 +12,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
