ce24d5206c8c778b9c68613b652325b32279d9f3
Alistair Blair <ablair08@me.com>
2020-04-15 13:55:21 +0100

    Add styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+
+import '@aws-amplify/ui/dist/style.css';
 Amplify.configure(aws_exports);
 
 class App extends Component {
