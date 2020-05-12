8265022373a632e5923ad96f3afc29daf1a04288
Yuriy Vakulik <yuriy.vakulik@just-eat.com>
2020-05-12 15:22:56 +0300

    move api to separate config

---
+++ src/api/aws-config.js
@@ -0,0 +1,11 @@
+const awsconfig = {
+    "API": {
+        "endpoints": [
+            {
+                "name": "Impressions",
+                "endpoint": "https://4eoh1izh32.execute-api.us-east-1.amazonaws.com/beta"
+            }
+        ]
+    }
+}
+export default awsconfig;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Amplify from 'aws-amplify';
 import aws_exports from '../aws-exports';
+import api_config from '../api/aws-config';
 import Impress from './Impress/Impress';
 
-Amplify.configure(aws_exports);
+Amplify.configure({...aws_exports, ...api_config});
 
 class App extends Component {
 
