0d179e9058b0b318c4c18871f05a0894e6bf80b0
hspain <hspain@gmail.com>
2020-07-09 13:20:47 -0700

    Update index.js
    test changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Test comment
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
