95e0d855c769c9da3ba5bf9158b309267acd58a9
rkalamaro <roy@scalesec.com>
2020-07-15 13:08:39 -0700

    changing default app index

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Sample js page for ScaleSec demo
           </p>
           <a
             className="App-link"
