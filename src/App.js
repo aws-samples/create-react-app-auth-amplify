a588be65053c087cc7c49b3d56f0536a9ea36f7d
nhowlandatl <nhowland@outlook.com>
2020-08-13 15:11:22 -0400

    changed amplify entry point

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import SearchForm from "./components/SearchForm";
 import GroceryItemResults from "./components/GroceryItemResults";
 import AboutUs from "./components/AboutUs";
 import Fridge from "./components/Fridge";
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 function App() {
   return ( 
