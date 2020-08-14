ae74f9d7fdbbf0b50f907aea64c5cbc45629912e
nhowlandatl <nhowland@outlook.com>
2020-08-14 18:32:56 -0400

    added a switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Route
+  Route, Switch
 } from "react-router-dom";
 import './App.css';
 import AWSLogin from "./components/AWSLogin";
@@ -15,12 +15,16 @@ function App() {
   return ( 
     <div className="App">
       <NavbarFixed />  
+      <Switch>
+
+      
       <Route exact path="/" component={FullPageIntroWithFixedNavbar} /> 
       <Route path="/SearchForm" component={SearchForm} />
       <Route path="/login" component={AWSLogin} />
       <Route path="/about" component={AboutUs} /> 
       <Route path="/MyFridge" component={Fridge} />
       <GroceryItemResults />
+      </Switch>
     </div>
   );
 }
