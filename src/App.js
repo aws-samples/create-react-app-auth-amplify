4dcfe1e58329080b543c5b57283789373bd50e2b
nhowlandatl <nhowland@outlook.com>
2020-08-14 18:19:23 -0400

    trying to figure out why Search won't render

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <NavbarFixed />  
       <Route exact path="/" component={FullPageIntroWithFixedNavbar} /> 
-      <Route exact path="/SearchForm" component={SearchForm} />
+      <Route path="/SearchForm" component={SearchForm} />
       <Route path="/login" component={AWSLogin} />
       <Route path="/about" component={AboutUs} /> 
       <Route path="/MyFridge" component={Fridge} />
