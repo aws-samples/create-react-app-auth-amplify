f3bf47db7cc908344579ef94476cce7b6c4a223a
Saniya Anush Shaikh <51195476+sanu29@users.noreply.github.com>
2020-09-15 04:23:51 -0700

    final

---
+++ src/.prettierrc.js
@@ -0,0 +1,11 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	'editor.formatOnSave': true,
+	// printWidth: 80,
+	proseWrap: 'always',
+};
