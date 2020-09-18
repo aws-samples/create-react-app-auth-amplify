000367b0c657578259c6bb4e2065785916727195
robanstee <robanstee@gmail.com>
2020-09-18 12:35:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
+      <p>HELLO WORLD</p>
+        <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
