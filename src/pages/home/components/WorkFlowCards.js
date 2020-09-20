8d34b899b528277a4dcdb6cc43595d30743c638b
pranav prajapati <pranav0492@gmail.com>
2020-09-20 20:24:35 +0530

    showing workflow created on home page

--- src/pages/home/components/WorkFlowCards.js
+++ src/pages/home/components/WorkFlowCards.js
@@ -2,14 +2,27 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 import Header from "./Header";
 import WorkFlowCard from "./WorkFlowCard";
-const WorkFlowCards = () => (
-  <Grid container direction="row" spacing={1}>
-    {[1, 2, 3].map((item) => (
-      <Grid item>
-        <WorkFlowCard />
-      </Grid>
-    ))}
-  </Grid>
-);
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  taskContainer: {
+    margin: "20px 0px",
+    position: "relative",
+  },
+}));
+const WorkFlowCards = ({ workflows }) => {
+  console.log("workflows: ", workflows);
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      {workflows.map(({ id, workFlowName }) => (
+        <Grid xs={3} className={classes.taskContainer} item key={id}>
+          <WorkFlowCard workFlowName={workFlowName} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 export default WorkFlowCards;
