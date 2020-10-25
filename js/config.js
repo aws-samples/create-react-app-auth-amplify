8c1afa0c2f84fc4ff87bc9b0809a08a5f3a31818
Yannickvwee <yannickvanwee@gmail.com>
2020-10-25 19:56:05 +0100

    config.js change for cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_n5YnJw1Hq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'i1db49b3r9et82mnrdfgtdgir', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
