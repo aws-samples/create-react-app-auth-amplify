a156cfca3a504a1151e8871ce360785e17378ade
Jacob Goren <jacobgoren@Jacobs-MacBook-Pro.local>
2020-10-26 20:16:27 -0700

    change1

--- src/App.js
+++ src/App.js
@@ -10,23 +10,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit! <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello World!</h1>
       </div>
     );
   }
 }
+
 
 export default withAuthenticator(App, true);
