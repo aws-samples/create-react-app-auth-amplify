e66d051fe500972fe1f76e34ffda3869dd598a9e
This-way-up <dkernan2015@icloud.com>
2020-12-23 15:03:20 -0800

    Update
    update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            MET
           </a>
         </header>
       </div>
