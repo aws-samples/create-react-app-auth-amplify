724d0ff7b00a3b7d8cb464071ff050b35fa93f40
This-way-up <dkernan2015@icloud.com>
2020-12-26 04:40:37 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Art
+            Artworks
           </p>
           <a
             className="App-link"
