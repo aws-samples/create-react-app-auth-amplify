5b7d7092034e6e6b4633c88708b0af04ed6e47d3
JonLittle <jon@themodernlogicgroup.com>
2021-01-30 08:36:57 -0800

    updates
    added video to main page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +14,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/B5mimngffGQ?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </p>
           <a
             className="App-link"
