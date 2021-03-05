7780c0e953bd8ace19ea0470b995cd197a9da35b
sgopwani <79894223+sgopwani@users.noreply.github.com>
2021-03-04 21:51:02 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+//Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
