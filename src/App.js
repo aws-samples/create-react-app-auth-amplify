026c08e16c8e3f2ab73f8dd4d1a6657ca3c151f8
sgopwani <79894223+sgopwani@users.noreply.github.com>
2021-03-08 21:38:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './Providers.jpg';
+//import logo from './Providers.jpg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
