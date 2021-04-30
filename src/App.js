6b810d74825d4b8a1583e88ceb10acd991e2e2f3
JemaaWassim <jemaa.wassim@outlook.fr>
2021-04-30 10:38:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            hello from PR
           </a>
         </header>
       </div>
