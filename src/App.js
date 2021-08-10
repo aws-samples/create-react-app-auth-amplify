e50e252909cf9bfd2e4345abc70586eacd6534c2
Nathan Alexander <CudaGeek@users.noreply.github.com>
2021-08-10 14:10:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ class App extends Component {
       <div className="App">
         <AmplifySignOut />
         <header className="App-header">
-        embedDashboard()
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to rela.
