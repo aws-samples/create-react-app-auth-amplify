b5df61480d34204a654040f58072eba01d377951
Nathan Alexander <CudaGeek@users.noreply.github.com>
2021-08-10 14:53:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to rela.
+            Hello world <code>src/App.js</code> and save to rela.
           </p>
           <a
             className="App-link"
