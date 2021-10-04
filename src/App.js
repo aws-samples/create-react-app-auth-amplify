5e6a2e00299574f6efeda2d0898ab8b908f8cd46
Gabriel <gabriel.motta@r3tecnologia.com>
2021-10-03 23:20:54 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -12,18 +12,7 @@ class App extends Component {
       <div className="App">
         <AmplifySignOut />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+       <h1>Teste</h1>
         </header>
       </div>
     );
