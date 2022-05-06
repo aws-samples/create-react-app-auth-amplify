479d50036edf67227278af126eca81bf230418ca
kelsej1 <30466699+kelsej1@users.noreply.github.com>
2022-05-06 13:50:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,16 +14,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload if you'd like.
+            Hi Ernie
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
