07736bcb04886f12d20391458cb5b61b18ffe9cc
kelsej1 <30466699+kelsej1@users.noreply.github.com>
2022-05-06 15:52:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,13 +16,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+          <a>
+            Yo yo yo 
           </a>
         </header>
       </div>
